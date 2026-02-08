ce42d3aaea6aaf147f379ed4f90cc4275c4a927f
adam <adt309@gmail.com>
2026-02-08 12:58:53 +0000

    Updated UI to return reuqired pot size and annual deposits

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -22,13 +22,12 @@ export class App {
   });
 
   readonly form = this.fb.group({
-    retirement_age: [65, [Validators.required, Validators.min(0)]],
-    annual_real_return: [0.04, [Validators.required]],
-    monthly_drawdown: [2500, [Validators.required, Validators.min(0)]],
+    age_now: [30, [Validators.required, Validators.min(0)]],
+    starting_pot: [75000, [Validators.required, Validators.min(0)]],
     death_age: [90, [Validators.required, Validators.min(1)]],
-    target_pot: [500000, [Validators.required, Validators.min(0)]],
-    starting_pot: [20000, [Validators.required, Validators.min(0)]],
-    years_until_retirement: [30, [Validators.required, Validators.min(1)]]
+    annual_real_return: [0.04, [Validators.required]],
+    monthly_drawdown: [4000, [Validators.required, Validators.min(0)]],
+    retirement_age: [57, [Validators.required, Validators.min(0)]],
   });
 
   submit(): void {
@@ -48,13 +47,12 @@ export class App {
 
     this.http
       .post<CalculateResponse>(url, {
+        age_now: formValue.age_now,
+        starting_pot: formValue.starting_pot,
+        death_age: formValue.death_age,
         retirement_age: formValue.retirement_age,
         annual_real_return: formValue.annual_real_return,
-        monthly_drawdown: formValue.monthly_drawdown,
-        death_age: formValue.death_age,
-        target_pot: formValue.target_pot,
-        starting_pot: formValue.starting_pot,
-        years_until_retirement: formValue.years_until_retirement
+        monthly_drawdown: formValue.monthly_drawdown
       })
       .subscribe({
         next: (data) => {
@@ -86,5 +84,4 @@ interface CalculateResponse {
   required_pot_at_retirement: number;
   required_annual_contribution: number;
   note?: string;
-  contribution_note?: string;
 }
